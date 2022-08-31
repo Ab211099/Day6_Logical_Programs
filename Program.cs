a1e1b3e053f062d37ab21559f72168d8f482e3ad
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-31 17:10:10 +0530

    [Abhi]Added: Prime number

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical program");
-            Console.WriteLine("Enter the program number \n 1.Generate Fibonacci number \n 2. Perfect Number");
+            Console.WriteLine("Enter the program number \n 1.Generate Fibonacci number \n 2. Perfect Number \n 3.Prime Number");
             int optioncheck = Convert.ToInt32(Console.ReadLine());
 
             switch (optioncheck)
@@ -15,6 +15,9 @@
                     break;                
                 case 2:
                     Perfect_Number.Perfectnumber();
+                    break; 
+                case 3:
+                    Prime_number.FindPrime();
                     break;
             }
         }
