cbdd4c08051c0c4cb8007700b0f3d858ddf3dd00
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-31 16:55:12 +0530

    [Abhi]Added: Perfect number

---
+++ Perfect_Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Perfect_Number
+    {
+        public static void Perfectnumber()
+        {
+            int number, sum = 0, n;
+            Console.Write(" enter the number ");
+            number = Convert.ToInt32(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+            }
+          
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical program");
-            Console.WriteLine("Enter the program number \n 1.Generate Fibonacci number");
+            Console.WriteLine("Enter the program number \n 1.Generate Fibonacci number \n 2. Perfect Number");
             int optioncheck = Convert.ToInt32(Console.ReadLine());
 
             switch (optioncheck)
             {
                 case 1:
                     Fibonacci.fibonoceiNumber();
+                    break;                
+                case 2:
+                    Perfect_Number.Perfectnumber();
                     break;
             }
         }
